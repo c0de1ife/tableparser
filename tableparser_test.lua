4cef5fd7f803c07e8646e08c2b16ddfa63f2701d
bin <38349899+c0de1ife@users.noreply.github.com>
2019-04-12 18:19:20 +0800

    add computational results to test file.

--- tableparser_test.lua
+++ tableparser_test.lua
@@ -9,3 +9,49 @@ parser.dump(targetString)
 local restoreTable = parser.decode(targetString)
 print("\nrestoreTable:")
 parser.dump(restoreTable)
+
+-- results:
+--[[
+sourceTable:
+{
+  1=1
+  2=3
+  b={
+    1=1.0
+    2={
+      c=abc
+      a=3
+    }
+    3={
+      1=true
+      2=false
+      3=true
+      4=true
+    }
+  }
+  true=12.5
+}
+
+targetString:
+{[1]=1,[2]=3,b={1.0,{c="abc",a=3},{true,false,true,true}},[true]=12.5}
+
+restoreTable:
+{
+  b={
+    1=1.0
+    2={
+      a=3
+      c=abc
+    }
+    3={
+      1=true
+      2=false
+      3=true
+      4=true
+    }
+  }
+  1=1
+  2=3
+  true=12.5
+}
+]]
